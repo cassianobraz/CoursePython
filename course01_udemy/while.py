6f0491c4b31c7f8a8b0d531f8956fe91feab9a2a
Cassiano Pereira Braz <cassiano.braz@dcoimbra.com.br>
2025-10-13 17:22:16 -0400

    feat: while and for

---
+++ course01_udemy/while.py
@@ -0,0 +1,43 @@
+# count = 0
+
+# while count <= 100:
+#     count += 1
+
+#     if count % 2 == 0:
+#         continue
+
+#     print(f"Apenas impar: {count}")
+
+#     if count == 41:
+#         break
+
+# qtd_rows = 5
+# qtd_columns = 5
+
+# line = 1
+# while line <= qtd_rows:
+#     column = 1
+#     while column <= qtd_columns:
+#         print(line, column)
+#         column += 1
+#     line += 1
+
+# print('Finish')
+
+# nome = 'Cassiano Pereira'
+
+# contador = 0
+# new_name = ''
+# while contador < len(nome):
+#     letra = nome[contador]
+#     new_name += f'{letra}*'
+#     contador += 1
+
+# print(new_name)
+
+# texto = 'Python'
+
+# novo_texto = ''
+# for i in texto:
+#     novo_texto += f'*{i}'
+# print(novo_texto)
