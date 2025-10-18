0aee827a98e3f0646da56e4de3dfb1880c6a75db
cassianobraz <cassanopb@gmail.com>
2025-10-18 00:06:46 -0400

    feat: new module raise

--- course01_udemy/concepts/tryexception.py
+++ course01_udemy/concepts/tryexception.py
@@ -1,13 +1,22 @@
 # Try, except, else e finally
 
+# try:
+#     a = 18
+#     # b = 0
+#     c = a / b
+# except ZeroDivisionError:
+#     print("Dividiu por zero.")
+# except NameError:
+#     print("Nome não está definido")
+
+
+# print()
+
 try:
-    a = 18
-    # b = 0
-    c = a / b
-except ZeroDivisionError:
-    print("Dividiu por zero.")
-except NameError:
-    print("Nome não está definido")
-
-
-print()
+  ...
+except:
+    ...
+else:
+    ...
+finally:
+    ...
